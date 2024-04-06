6a0e5134863b380ba9464e1892e00f06bf846621
nicktsan <nicktsang.manhay@gmail.com>
2024-04-06 00:08:44 -0700

    Added SiteFooter.vue

--- app.vue
+++ app.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen">
     <SiteHeader />
-    <!-- <NuxtWelcome /> -->
-    <!-- displays the current page -->
-    <main class ="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full">
       <NuxtPage />
     </main>
+    <SiteFooter />
   </div>
 </template>
