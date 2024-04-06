5fbaf0ad7fc7579f749c747af6a692270863fb08
nicktsan <nicktsang.manhay@gmail.com>
2024-04-05 23:08:25 -0700

    Added SiteHeader.vue and index.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <SiteHeader />
+    <!-- <NuxtWelcome /> -->
+    <!-- displays the current page -->
+    <main class ="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <NuxtPage />
+    </main>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+    <section>
+        <h1 class="text-5xl font-bold mt-20">👋🏻 Hello, I'm Nicholas Tsang!</h1>
+        <p class="text-base text-gray-900 p-2 italic">Software Developer</p>
+    </section>
+    <section class="flex flex-col md:flex-row">
+    <div class="md:w-3/4">
+        <h2 class="text-3xl font-bold mt-8">My Story</h2>
+        <p class="text-lg py-2">
+            With a solid foundation in quality assurance at Tata Consultancy Services, I bring a wealth of experience in 
+            ensuring flawless software performance. My transition to software development stems from a deep-seated passion 
+            for crafting innovative solutions that enrich user experiences. I possess the ability to adapt swiftly to any role 
+            or project. I am eager to leverage my skills and drive in software development to embark on a new and fulfilling 
+            career journey. Currently looking for software developer, software engineer, full stack developer, and back end 
+            developer roles that use C#, Python, Java, Go, React, or Typescript.
+        </p>
+    </div>
+    
+  </section>
+</template>
