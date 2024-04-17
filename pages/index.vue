2dcc6a96cbca443511514b418d6c7dd9d79d8dc7
nicktsan <nicktsang.manhay@gmail.com>
2024-04-16 17:16:02 -0700

    Added programming languages and technologies to index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,32 @@ const { data: posts } = await useAsyncData('latest-posts', () =>
         .limit(3)
         .find()
 )
+//   Programming Languages: C#, Java, Python, Typescript, Javascript, Go
+const programmingLanguages = [
+  { name: 'C#', icon: 'devicon:csharp' },
+  { name: 'Java', icon: 'devicon:java' },
+  { name: 'Python', icon: 'devicon:python' },
+  { name: 'Typescript', icon: 'devicon:typescript' },
+  { name: 'Javascript', icon: 'devicon:javascript' },
+  { name: 'Go', icon: 'devicon:go-wordmark' },
+]
+// Technologies: Github, MySQL, Amazon Web Services, Docker, Terraform, React, NextJS, PostGreSQL, Kubernetes, HTML, CSS, Tailwind CSS, NuxtJS, GraphQL
+const technologies = [
+    { name: 'GitHub', icon: 'devicon:github' },
+    { name: 'SQL', icon: 'devicon:mysql' },
+    { name: 'Amazon Web Services', icon: 'devicon:amazonwebservices-wordmark' },
+    { name: 'Docker', icon: 'devicon:docker' },
+    { name: 'Terraform', icon: 'devicon:terraform' },
+    { name: 'React', icon: 'devicon:react' },
+    { name: 'NextJS', icon: 'devicon:nextjs' },
+    { name: 'PostGreSQL', icon: 'devicon:postgresql' },
+    { name: 'Kubernetes', icon: 'devicon:kubernetes' },
+    { name: 'HTML', icon: 'devicon:html5' },
+    { name: 'CSS', icon: 'devicon:css3' },
+    { name: 'Tailwind CSS', icon: 'devicon:tailwindcss' },
+    { name: 'Nuxt', icon: 'devicon:nuxtjs' },
+    { name: 'GraphQL', icon: 'logos:graphql' },
+]
 </script>
 
 <template>
@@ -25,6 +51,24 @@ const { data: posts } = await useAsyncData('latest-posts', () =>
         </p>
     </div>
     <img src="~/assets/images/grad pic crop.png" class="w-1/2 md:max-w-sm p-8 mx-auto" />
+  </section>
+  <section>
+    <h2 class="text-3xl font-bold mt-8">Programming Languages</h2>
+    <div class="flex flex-row flex-wrap gap-4 my-2">
+            <p v-for="programmingLanguage in programmingLanguages" :key="programmingLanguage.name" class="p-1 border-2 rounded-lg">
+            <Icon :name="programmingLanguage.icon" size="1.5rem"/>
+            {{ programmingLanguage.name }}
+            </p>
+    </div>
+  </section>
+  <section>
+    <h2 class="text-3xl font-bold mt-8">Technologies</h2>
+    <div class="flex flex-row flex-wrap gap-4 my-2">
+            <p v-for="technology in technologies" :key="technology.name" class="p-1 border-2 rounded-lg">
+            <Icon :name="technology.icon" size="1.5rem"/>
+            {{ technology.name }}
+            </p>
+    </div>
   </section>
   <section>
     <h2 class="text-3xl font-bold mt-8">Latest Blog Posts</h2>
