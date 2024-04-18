69cac1147859553f3487fd73c2b0cafb8d0845e2
nicktsan <nicktsang.manhay@gmail.com>
2024-04-18 16:21:09 -0700

    Fixed github projects display

--- pages/index.vue
+++ pages/index.vue
@@ -40,18 +40,18 @@ const technologies = [
         <p class="text-base text-gray-900 p-2 italic">Software Developer</p>
     </section>
     <section class="flex flex-col md:flex-row">
-    <div class="md:w-3/4">
-        <h2 class="text-3xl font-bold mt-8">My Story</h2>
-        <p class="text-lg py-2">
-            I am a former quality assurance engineer who is aspiring to become a software engineer. My transition to software 
-            development stems from a deep-seated passion 
-            for crafting innovative solutions that enrich user experiences. I possess the ability to adapt swiftly to any role 
-            or project. I am eager to leverage my skills and drive in software development to embark on a new and fulfilling 
-            career journey. Currently looking for software developer, software engineer, full stack developer, and back end 
-            developer roles that use C#, Python, Java, Go, React, or Typescript.
-        </p>
-    </div>
-    <img src="~/assets/images/grad pic crop.png" class="w-1/2 md:max-w-sm p-8 mx-auto" />
+      <div class="md:w-3/4">
+          <h2 class="text-3xl font-bold mt-8">My Story</h2>
+          <p class="text-lg py-2">
+              I am a former quality assurance engineer who is aspiring to become a software engineer. My transition to software 
+              development stems from a deep-seated passion 
+              for crafting innovative solutions that enrich user experiences. I possess the ability to adapt swiftly to any role 
+              or project. I am eager to leverage my skills and drive in software development to embark on a new and fulfilling 
+              career journey. Currently looking for software developer, software engineer, full stack developer, and back end 
+              developer roles that use C#, Python, Java, Go, React, or Typescript.
+          </p>
+      </div>
+      <img src="~/assets/images/grad pic crop.png" class="w-1/2 md:max-w-sm p-8 mx-auto" />
     </section>
     <section>
       <h2 class="text-3xl font-bold mt-8">Programming Languages</h2>
